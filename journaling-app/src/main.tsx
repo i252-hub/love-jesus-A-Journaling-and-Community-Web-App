bfb295bd8a7484c1605113b901d511c671d44cc8
i252-hub <jushirarey@gmail.com>
2024-12-16 12:25:41 +0800

    chore: configured tailwind packages

--- journaling-app/src/main.tsx
+++ journaling-app/src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import Navbar from './components/Navbar'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Navbar
+    Journal='Journal'
+    Community = 'Community'
+    About = 'About'
+    SignIn = 'Sign-in' />
   </StrictMode>,
 )
--- journaling-app/tailwind.config.js
+++ journaling-app/tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: { customBrown: '#816F51',
+                customYellow: '#F6E3B5'
+       },
+       fontFamily: { roboto: ['Roboto', 'sans-serif'],
+                      belle: ['La Belle Aurore', 'cursive'],
+                      annie: ['Annie Use Your Telescope', 'cursive'],
+          },
+       screens: { 
+             'mobile': {'max': '641px'},
+             'tablet': {'max': '769px', 'min': '642px'},
+             'desktop': {'min': '770px'},
+          },
+    },
   },
   plugins: [],
 }
