622d4400d781e3939e2d9d52f499737db47b8e87
i252-hub <jushirarey@gmail.com>
2025-02-08 19:15:30 +0800

    feat: added local storage for entries on blessings page + prayerreq page

--- journaling-app/src/components/StorageTwo.tsx
+++ journaling-app/src/components/StorageTwo.tsx
@@ -1,8 +1,25 @@
-const Storage: React.FC = () => {
-return(
-    <>
-    </>
-)
+
+interface StorageTwoProps<T> {
+  storageKey: string;
+  setNotes: React.Dispatch<React.SetStateAction<T[]>>;
+  updateNote?: (index: number, newNote: { title: string; content: string }) => void;
+  updateNoteTwo?: (index: number, newNote: { text: string}) => void;
 }
 
-export default Storage;
+const StorageTwo= <T extends object>({ storageKey, setNotes, updateNoteTwo }: StorageTwoProps<T>) => {
+  const handleUpdateNote = (index: number, newNote: T) => {
+    setNotes((prevNotes) => {
+      const updatedNotes = prevNotes.map((note, i) => (i === index ? newNote : note));
+      localStorage.setItem(storageKey, JSON.stringify(updatedNotes));
+      return updatedNotes;
+    });
+  };
+
+  if (updateNoteTwo) {
+    updateNoteTwo = (index: number, newNote: { text: string }) => handleUpdateNote(index, newNote as T);
+  }
+
+  return null;  
+};
+
+export default StorageTwo;
