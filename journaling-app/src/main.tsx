3f3060b646a44be0602e2dda7bf917abed2a151b
i252-hub <jushirarey@gmail.com>
2025-02-20 12:54:16 +0800

    feat: imported CountProvider and AuthProvider to pass components globally

--- journaling-app/src/main.tsx
+++ journaling-app/src/main.tsx
@@ -4,10 +4,18 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes";
 import './index.css'
 
+
+import { CountProvider } from '../src/components/Context';
+import AuthProvider from './components/AuthContext';
+
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <AuthProvider>
+   <CountProvider>
    <RouterProvider router={router} />
+    </CountProvider>
+    </AuthProvider>
   </StrictMode>,
 )
